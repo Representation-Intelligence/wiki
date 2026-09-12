dea53507784c709d8b2bc3fdb6f9abe4f0df2643
shoa-lin <shoa_lin@outlook.com>
2026-09-12 15:13:17 +0800

    fix: serialize auth settings for mysql migrations

--- server/db/migrations/2.5.130.js
+++ server/db/migrations/2.5.130.js
@@ -7,7 +7,7 @@ exports.up = async knex => {
   if (!await knex('groups').where('id', 4).first()) await knex('groups').insert({ id: 4, name: 'Team Members', permissions: JSON.stringify([]), pageRules: JSON.stringify([]), isSystem: false, createdAt: new Date().toISOString(), updatedAt: new Date().toISOString() })
   await knex('groups').where('id', 2).update({ permissions: JSON.stringify(['read:pages', 'read:assets', 'read:comments']), pageRules: JSON.stringify(publicRules) })
   await knex('groups').where('id', 4).update({ permissions: JSON.stringify(['read:pages', 'read:source', 'read:history', 'read:assets', 'write:pages', 'write:assets']), pageRules: JSON.stringify(teamRules) })
-  await knex('authentication').where('key', 'local').update({ selfRegistration: true, domainWhitelist: { v: ['ewo.so'] }, autoEnrollGroups: { v: [4] } })
+  await knex('authentication').where('key', 'local').update({ selfRegistration: true, domainWhitelist: JSON.stringify({ v: ['ewo.so'] }), autoEnrollGroups: JSON.stringify({ v: [4] }) })
 }
 
-exports.down = knex => knex('authentication').where('key', 'local').update({ selfRegistration: false, domainWhitelist: { v: [] }, autoEnrollGroups: { v: [] } })
+exports.down = knex => knex('authentication').where('key', 'local').update({ selfRegistration: false, domainWhitelist: JSON.stringify({ v: [] }), autoEnrollGroups: JSON.stringify({ v: [] }) })
